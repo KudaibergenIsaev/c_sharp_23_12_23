2ece8a2f26766e2d62a6b625e4d79fd95f8f3347
isaev_kudaibergen <isaev.kudaibergen@mail.ru>
2023-12-23 22:54:51 +0600

    classWork task 1_1

---
+++ lesson_1/1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 25;
+int b = 5;
+
+if (b * b == a)
+    Console.WriteLine("true");
+else
+    Console.WriteLine("false");
+
+Console.Write("Enter a number: ");
+string str1 = Console.ReadLine()!;
+
+Console.WriteLine(str1 + str1);
+
+int num1 = int.Parse(str1);
+Console.WriteLine(num1 + num1);
+
+Console.Write("Enter a number: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(num1 * num1);
